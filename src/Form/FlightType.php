b831123389359f425faebeaafb93cd84ab098377
Alexandre Corrette <alexandrecorrette@gmail.com>
2021-02-08 14:48:22 +0100

    add some responsive

--- src/Form/FlightType.php
+++ src/Form/FlightType.php
@@ -3,10 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Flight;
+use Doctrine\DBAL\Types\ArrayType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class FlightType extends AbstractType
 {
@@ -14,10 +17,14 @@ class FlightType extends AbstractType
     {
         $builder
             ->add('flightNumber', TextType::class, [
+                'label'=>'Numéro de vol',
                 'attr' => ['placeholder' => 'ex: AF887'],
             ])
             ->add('departureCity', TextType::class)
             ->add('arrivalCity', TextType::class)
+            ->add('comments', TextType::class, [
+                'label'=>'Votre commentaire',
+            ])
         ;
     }
 
